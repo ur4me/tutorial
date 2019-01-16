a8525ded06d2298416c5e626c0e23c36cd6b230a
Steve Kim <30827345+ur4me@users.noreply.github.com>
2019-01-17 10:34:50 +1300

    Update tumor.R

--- tumor.R
+++ tumor.R
@@ -4,9 +4,7 @@ df <- data.frame(tumor.size = runif(100, 0, 7))
 df$cancer <- ifelse(df$tumor.size > 4, 'yes','no')
 df$tumor.size <- df$tumor.size * runif(100, 1,2)
 
-a <- df %>% filter(cancer == 'yes')
-b <- df %>% filter(cancer == 'no')
-
+#plot
 #http://t-redactyl.io/blog/2016/04/creating-plots-in-r-using-ggplot2-part-10-boxplots.html
 library(ggplot2)
 ggplot(df, aes(x = cancer, y = tumor.size)) +
